2579568681d11f8a53d81f40af19d6851e53f0fa
Wei <wwei6@vt.edu>
2021-02-09 13:21:39 -0500

    Q12.3

--- allAnswers.cpp
+++ allAnswers.cpp
@@ -192,14 +192,42 @@ string pick(double p) {
   else return "Game1 and Game2 have the same probability\n";
 }
 
-//Q12.1Last K Lines: Write a method to print the last Klines of an input file
-//using C++.
-//string printK(int k, string s) {
-//  int size_line = 0;
-//  for (auto c : s) {
-//    if(c == '\n') size_line += 1;
-//  }
-//}
+//Q12.1
+void PrintLK(char * filename, const int k) {
+  std::ifstream file(filename);
+  if (file.is_open()) {
+      std::string line1, line2;
+      int cnt1 = 0, cnt2 = 0;
+      while (std::getline(file, line1) && !file.eof()) {
+          ++cnt1;
+      }
+      while (std::getline(file, line2) && !file.eof()) {
+          ++cnt2;
+          while (cnt2 == cnt1 - k) printf("%s", line2.c_str());
+      }
+      file.close();
+  }
+}
+
+//Q12.2
+void reverse(char * str) {
+  int size = 0;
+  while ((*str)!= '\0') {
+    ++size;
+    ++str;
+  }
+  str -= 1;
+  char c[size+1];
+  for (int i=0; i != size && (*str)!= '\0' && str != nullptr; --str, ++i) {
+    c[i] = *str;
+  }
+  str += 1;
+  c[size+1] = '\0';
+  for (int i = 0 ; i != size; ++str, ++i){
+    *str = c[i];
+  }
+}
+
 
 
 int main() {
@@ -337,9 +365,34 @@ cout << pick(p) << endl;
 //out logically and then write a computer simulation of it.
 
 
+*/
+
+
+
+
+
+/*
 //Q12.1 Last K Lines: Write a method to print the last Klines of an input file
 //using C++.
 
+//Q12.2 implement a function void reverse(char * str) in C or C++ which reverses
+//a null-terminated string
+char cstring1[] = {"wei"};
+char cstring2[4] = {'w', 'e', 'i'};
+char cstring3[4] = {"wei"};
+char cstring4[4] = {'w', 'e', 'i', '\0'};
+
+reverse(cstring1);
+reverse(cstring2);
+reverse(cstring3);
+reverse(cstring4);
+cout << cstring1 << " " << cstring2  << " " << cstring3  << " " << cstring4 << endl;
+
+*/
+
+//Q12.3 Hash Table vs STL Map: Compare and contrast a hash table and an STL
+//map. How is a hash table implemented? If the number of inputs is small, which
+//data structure options can be used instead of a hash table?
 
 
 
