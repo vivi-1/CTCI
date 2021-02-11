6b8b20451be99037c752120d8f7ff08c1ee59c72
Wei <wwei6@vt.edu>
2021-02-11 11:53:07 -0500

    copy questions

--- chapter12.cpp
+++ chapter12.cpp
@@ -64,7 +64,42 @@ cout << cstring1 << " " << cstring2  << " " << cstring3  << " " << cstring4 << e
 //Q12.3 Hash Table vs STL Map: Compare and contrast a hash table and an STL
 //map. How is a hash table implemented? If the number of inputs is small, which
 //data structure options can be used instead of a hash table?
+//Hash table vs Standard Template Library (STL)
 
+//Q12.4 Virtual Functions: How do virtual functions work in C++?
+
+//Q12.5 Shallow vs Deep Copy: What is the difference between deep copy and
+//shallow copy? Explain how you would use each.
+
+//Q12.6 Volatile: What is the significance of the keyword "volatile" in C?
+
+//Q12.7 Virtual Base Class: Why does a destructor in base class need to be
+//declared virtual?
+
+//Q12.8 Copy Node: Write a method that takes a pointer to a Node structure as a
+//parameter and returns a complete copy of the passed in data structure.
+//The Node data structure contains two pointers to other Nodes.
+
+
+// Q12.9 Smart Pointer: Write a smart pointer class. A smart pointer is a data
+//type, usually implemented with templates, that simulates a pointer while also
+//providing automatic garbage collection. It automatically counts the number of
+//references to a SmartPointer<T*>object and frees the object of type T when the
+//reference count hits zero.
+
+//12.10 Malloc: Write an aligned malloc and free function that supports
+//allocating memory such that the memory address returned is divisible by a
+//specific power of two.
+//EXAMPLE
+//align _mallo c (ieee, 128) will return a memory address that is a multiple of
+//128 and that points to memory of size 1000 bytes.
+//aligned_free() will free memory allocated by align_malloc.
+
+
+
+//Q12.11 20 Alloc: Write a function in C called my2DAlloc which allocates a
+//two-dimensional array. Minimize the number of calls to malloc and make sure
+//that the memory is accessible by the notation arr[iJ [j].
 
 
 
