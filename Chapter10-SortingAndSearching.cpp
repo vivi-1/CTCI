9b2cdc62ff7491ae7bec8d3c5cec9afa06952830
vivi-1 <wwei6@vt.edu>
2021-02-21 20:51:09 -0500

    Chapter10 Q10.2

--- Chapter10-SortingAndSearching.cpp
+++ Chapter10-SortingAndSearching.cpp
@@ -12,12 +12,38 @@ vector<int> SortedMerge(vector<int> v1, vector<int> v2) {
   return v1;
 }
 
+//Q10.2 Group Anagrams: Write a method to sort an array of strings so that all
+//the anagrams are next to each other.
+class HashTable{
+private:
+  vector<string> *table;
+  int size_of_vector;
+  int getHash(string key) {
+    return 
+  }
+}
+
+
+vector<int> SortAnagrams(vector<string> v) {
+  vector<string> tempV;
+  for (auto i : v) {
+    temp = i;
+    sort(temp.begin(), temp.end());
+    tempV.push_back(i);
+
+  }
+
+}
+
 int main(){
+/*
 //Q10.1
-vector<int> v1 = {1, 2, 3, 4, 5, 6};
-vector<int> v2 = {4, 5, 6, 7, 8, 9};
-v1 = SortedMerge(v1, v2);
-for(int i = 0; i!= v1.size(); i++) cout << v1[i] << endl;
+  vector<int> v1 = {1, 2, 3, 4, 5, 6};
+  vector<int> v2 = {4, 5, 6, 7, 8, 9};
+  v1 = SortedMerge(v1, v2);
+  for(int i = 0; i!= v1.size(); i++) cout << v1[i] << endl;
+*/
+
 
   return 0;
 }
