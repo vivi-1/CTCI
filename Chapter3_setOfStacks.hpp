d68d661e75052a7ba4e5e394ea173b0a9cb9d52f
vivi1 <wwei6@vt.edu>
2021-02-25 22:54:58 -0500

    setOfStacks.hpp

---
+++ Chapter3_setOfStacks.hpp
@@ -0,0 +1,18 @@
+#ifndef CHAPTER3_SETOFSTACKS_HPP
+#define CHAPTER3_SETOFSTACKS_HPP
+
+#include<iostream>
+#include<vector>
+#include "Chapter3_stack.hpp"
+using namespace std;
+
+class setOfStacks{
+  public:
+    setOfStacks(stack);
+    setOfStacks(const setOfStackstack&);
+    ~setOfStacks();
+
+
+}
+
+#endif //CHAPTER3_SETOFSTACKS_HPP
